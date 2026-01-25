a9a26e0675cfc3a08f2482227fe8a8b62c80dab2
oktaflat <oktasharp@gmail.com>
2026-01-25 08:31:53 +0100

    change landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <main>
-            <div className="hero">
+            {/* <div className="hero">
                 <div className="upper">
                     <h1 className="white">original PDF in, translated PDF out</h1>
                     <p className="white">MangoPanel will effortlessly translate the manga <span className="purple">with full context of the previous pages using a machine language model around translating the way a writer would translate.</span> Translated pdfs can be downloaded, saved and read using our PDF reader.</p>
@@ -24,9 +24,29 @@ export default function Home() {
             </div>
             <div>
                 
+            </div> */}
+            <div id="landing-content">
+                <div id="text-content">
+                    <h1>original PDF in, translated PDF out.</h1>
+                    <p>MangoPanel will effortlessly translate the manga <span>with full context of the previous pages using a machine language model around translating the way a writer would translate.</span> Translated pdfs can be downloaded, saved and read using our PDF reader.</p>
+                    <Link href="/register">
+                        <div id="start-button">
+                            GET STARTED<span>!!</span>
+                        </div>
+                    </Link>
+                </div>
+                <div id="manga-preview">
+                    <div id="img-gradient">
+                    <Image className="img" 
+                            src="/placeholder.webp"
+                            width={333}
+                            height={500}
+                            alt="placeholder"
+                        />
+                    </div>
+                    <p>PREVIEW</p>
+                </div>
             </div>
         </main>
     );
 }
-
-// background image source: banishment (pixiv)
