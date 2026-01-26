f0316fff0562cbf2feb3352886e4f1616356c916
Artur Tomasiak <arturkrystiantomasiak@gmail.com>
2026-01-26 22:25:39 +0100

    visual improvements

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
                 <div id="text-content">
                     <h1>original PDF in, translated PDF out.</h1>
                     <p>MangoPanel will effortlessly translate the manga <span>with full context of the previous pages using a machine language model around translating the way a writer would translate.</span> Translated pdfs can be downloaded, saved and read using our PDF reader.</p>
+                    <p>This is a student project, do not take it seriously.</p>
                     <Link href="/register">
                         <div id="start-button">
                             GET STARTED<span>!!</span>
@@ -16,10 +17,10 @@ export default function Home() {
                 <div id="manga-preview">
                     <div id="img-gradient">
                     <Image className="img" 
-                            src="/placeholder.webp"
+                            src="/preview.png"
                             width={333}
                             height={500}
-                            alt="placeholder"
+                            alt="preview of the translation"
                         />
                     </div>
                     <p>PREVIEW</p>
